c9a2c88d05e56dd22995b73962575494271b4718
K25BCM <>
2019-06-07 12:08:09 +0200

    Repositorios finales

--- back/src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOfferRepository.java
+++ back/src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOfferRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Offer;
-import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.repository.OfferRepository;
 
 
