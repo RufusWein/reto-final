894ed031885e5e64b39ac60bbafb665e8c650dea
guilleiba93 <sgsguille1993@gmail.com>
2019-06-07 14:11:55 +0200

    Servicio

--- front/src/app/ofert/ofert.service.ts
+++ front/src/app/ofert/ofert.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Ofert } from './ofert';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertService {
-
+  
   constructor(private httpClient: HttpClient) { }
-
+  
   getOfert() {
     return this.httpClient.get<Ofert[]>('')
+  }
+  addOfert(oferts : Ofert): Observable<any>{ {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    return this.httpClient.post<Ofert>('', oferts, {headers});
   }
 }
