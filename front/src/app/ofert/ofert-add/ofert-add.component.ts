58db0a3ecce400b0d537ed9633b5fbaa807dddfe
ismlunzam <ismlunzam@gmail.com>
2019-06-07 13:29:01 +0200

    html add

--- front/src/app/ofert/ofert-add/ofert-add.component.ts
+++ front/src/app/ofert/ofert-add/ofert-add.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ofert-add.component.css']
 })
 export class OfertAddComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
