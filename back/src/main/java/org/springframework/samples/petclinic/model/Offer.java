4317104741c426d29105b1f709d6aaa1accca1ee
irene <irenenarbonasiles@gmai.com>
2019-06-07 10:25:26 +0200

    Entidad Oferta

---
+++ back/src/main/java/org/springframework/samples/petclinic/model/Offer.java
@@ -0,0 +1,64 @@
+package org.springframework.samples.petclinic.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "offers")
+public class Offer extends BaseEntity{
+	
+	@Column(name = "title")
+	@NotEmpty
+	private String title;
+	
+	@Column(name = "description")
+	@NotEmpty
+	private String description;
+	
+	@Column(name = "discount")
+	@NotEmpty
+	private double discount;
+	
+	@Column(name = "expireDate")
+	@NotEmpty
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date expireDate;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public Date getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
+	
+	
+	
+	
+}
