7c92db9f85672c746e89cff0b4230a25b46e6ea7
K25BCM <>
2019-06-07 11:13:33 +0200

    Repository

---
+++ back/src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataOfferRepository.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.repository.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.samples.petclinic.repository.OfferRepository;
+
+@Profile("spring-data-jpa")
+public interface SpringDataOfferRepository  extends OfferRepository, Repository<Offer, Integer> {
+
+	@Override
+	@Query("SELECT offer FROM Offer offer WHERE offer.id=:id")
+	public Offer findById(@Param("id") int id);
+	
+}
