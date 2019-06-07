70c28f10b1b278b140b3a92bcf3066c1f2f6e17a
irene <irenenarbonasiles@gmai.com>
2019-06-07 14:36:30 +0200

    OfferControler con PUT

--- back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
+++ back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
@@ -3,16 +3,22 @@ package org.springframework.samples.petclinic.rest;
 import java.time.LocalDate;
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.model.Offer;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/offers")
@@ -46,6 +52,22 @@ public class OfferRestController {
 			return new ResponseEntity<Collection<Offer>>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Collection<Offer>>(offers, HttpStatus.OK);
+	}
+	
+	// @PreAuthorize( "hasRole(@roles.OWNER_ADMIN)" )
+	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Offer> addOffer(@RequestBody @Valid Offer offer, BindingResult bindingResult,
+			UriComponentsBuilder ucBuilder) {
+		BindingErrorsResponse errors = new BindingErrorsResponse();
+		HttpHeaders headers = new HttpHeaders();
+		if (bindingResult.hasErrors() || (offer == null)) {
+			errors.addAllErrors(bindingResult);
+			headers.add("errors", errors.toJSON());
+			return new ResponseEntity<Offer>(headers, HttpStatus.BAD_REQUEST);
+		}
+		this.clinicService.saveOffer(offer);
+		headers.setLocation(ucBuilder.path("/api/offers/{id}").buildAndExpand(offer.getId()).toUri());
+		return new ResponseEntity<Offer>(offer, headers, HttpStatus.CREATED);
 	}
 	
 	
